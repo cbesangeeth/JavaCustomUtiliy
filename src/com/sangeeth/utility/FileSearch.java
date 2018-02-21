3c3f7c4c220d673d74812aa23781f89b35539b98
Sangeeth <cbe.sangeeth@gmail.com>
2018-02-21 22:23:41 +0530

    added file copy and commented insert into db

--- src/com/sangeeth/utility/FileSearch.java
+++ src/com/sangeeth/utility/FileSearch.java
@@ -4,6 +4,9 @@
 package com.sangeeth.utility;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,18 +16,22 @@ import java.util.Map;
  */
 public class FileSearch {
 
+	// Output directory
+	private static String outputDirectory = "C:/Users/Elcot/Desktop/destination";
+
+	// Search string
+	private static String searchString = "browser";
+
 	public static void main(String[] args) {
 		// Parent Directory
 		String parentDirectory = "C:/Users/Elcot/Desktop/Gitter-master/Gitter-master/node_modules/@angular/animations";
-		// Search string
-		String searchString = "browser";
 
-		// File object 
+		// File object
 		File myDirectory = new File(parentDirectory);
 		Map<String, String> myMap = new HashMap<String, String>();
-		Map<String, String> myFinalMap = new HashMap<String, String>();
+		// Map<String, String> myFinalMap = new HashMap<String, String>();
 
-		// File Look up 
+		// File Look up
 		if (myDirectory.isDirectory()) {
 			File arr[] = myDirectory.listFiles();
 
@@ -37,21 +44,22 @@ public class FileSearch {
 		}
 
 		// File Search and insert it into DB
-		if (myMap.size() > 0) {
-			FileDAO myDAO = new FileDAO();
-			for (Map.Entry<String, String> entry : myMap.entrySet()) {
-				if (entry.getValue().contains(searchString)) {
-					System.out.println(entry.getValue());
-					/* -------------IMPORTANT----------------------------------- 
-					 * 		To insert it into the DB
-					 * -------------IMPORTANT-----------------------------------*/
-					// myDAO.insertIntoDB(entry.getKey(), entry.getValue());
-				}
-			}
-			System.out.println("**********************************************");
-			System.out.println("End of Search and insertion");
-			System.out.println("**********************************************");
-		}
+		/*
+		 * if (myMap.size() > 0) { // FileDAO myDAO = new FileDAO(); for
+		 * (Map.Entry<String, String> entry : myMap.entrySet()) { if
+		 * (entry.getValue().contains(searchString)) {
+		 * System.out.println(entry.getValue());
+		 * 
+		 * -------------IMPORTANT----------------------------------- To insert
+		 * it into the DB
+		 * -------------IMPORTANT-----------------------------------
+		 * 
+		 * // myDAO.insertIntoDB(entry.getKey(), entry.getValue()); } }
+		 * System.out.println("**********************************************");
+		 * System.out.println("End of Search and insertion");
+		 * System.out.println("**********************************************");
+		 * }
+		 */
 	}
 
 	/**
@@ -60,27 +68,44 @@ public class FileSearch {
 	private static void deeperSearch(File[] arr, int index, int level, Map aMap) {
 		if (index == arr.length)
 			return;
-		/* -------------IMPORTANT----------------------------------- 
-		 * 		uncomment this for printing in console only 
-		 * -------------IMPORTANT-----------------------------------*/
-		/*for (int i = 0; i < level; i++) {
-			System.out.print("\t");
-		}*/
+		/*
+		 * -------------IMPORTANT----------------------------------- uncomment
+		 * this for printing in console only
+		 * -------------IMPORTANT-----------------------------------
+		 */
+		/*
+		 * for (int i = 0; i < level; i++) { System.out.print("\t"); }
+		 */
 
 		if (arr[index].isDirectory()) {
-			/* -------------IMPORTANT----------------------------------- 
-			 * 		uncomment this for printing in console only 
-			 * -------------IMPORTANT-----------------------------------*/
-			//System.out.println("[" + arr[index].getName() + "]");
+			/*
+			 * -------------IMPORTANT-----------------------------------
+			 * uncomment this for printing in console only
+			 * -------------IMPORTANT-----------------------------------
+			 */
+			// System.out.println("[" + arr[index].getName() + "]");
 
 			// Recursive call
 			deeperSearch(arr[index].listFiles(), 0, level + 1, aMap);
 		} else if (arr[index].isFile()) {
-			aMap.put(arr[index].getAbsoluteFile(), arr[index].getName());
-			/* -------------IMPORTANT----------------------------------- 
-			 * 		uncomment this for printing in console only 
-			 * -------------IMPORTANT-----------------------------------*/
-			//System.out.println(arr[index].getAbsoluteFile());
+
+			if (arr[index].getName().contains(searchString)) {
+				aMap.put(arr[index].getAbsoluteFile(), arr[index].getName());
+				try {
+					// Copies file from src to target folder
+					Files.copy(arr[index].getAbsoluteFile().toPath(),
+							new File(outputDirectory + "/" + arr[index].getName()).toPath(),
+							StandardCopyOption.REPLACE_EXISTING);
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			/*
+			 * -------------IMPORTANT-----------------------------------
+			 * uncomment this for printing in console only
+			 * -------------IMPORTANT-----------------------------------
+			 */
+			// System.out.println(arr[index].getAbsoluteFile());
 		}
 		deeperSearch(arr, ++index, level, aMap);
 	}
